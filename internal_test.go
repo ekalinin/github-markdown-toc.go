a33a430b821c2261bda7ff067bcea49bf1b19710
Eugene Kalinin <e.v.kalinin@gmail.com>
2021-12-13 22:03:41 +0300

    Add Test_httpGetForbidden

--- internal_test.go
+++ internal_test.go
@@ -27,3 +27,20 @@ func Test_httpGet(t *testing.T) {
 		t.Error("\nGot :", got, "\nWant:", expected)
 	}
 }
+
+func Test_httpGetForbidden(t *testing.T) {
+	txt := "please, do not try"
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusForbidden)
+		_, err := fmt.Fprint(w, txt)
+		if err != nil {
+			println(err)
+		}
+	}))
+	defer srv.Close()
+
+	_, _, err := httpGet(srv.URL)
+	if err == nil {
+		t.Error("Should not not be nil")
+	}
+}

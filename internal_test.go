dd31b24259056166446fc02b75d34500306f7cbc
Eugene Kalinin <e.v.kalinin@gmail.com>
2021-12-13 22:15:00 +0300

    Add Test_httpPost

--- internal_test.go
+++ internal_test.go
@@ -2,8 +2,11 @@ package ghtoc
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
@@ -42,5 +45,48 @@ func Test_httpGetForbidden(t *testing.T) {
 	_, _, err := httpGet(srv.URL)
 	if err == nil {
 		t.Error("Should not not be nil")
+	}
+}
+
+func createTmp(content string) (string, error) {
+	tmpFile, err := ioutil.TempFile("", "example.*.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := tmpFile.Write([]byte(content)); err != nil {
+		tmpFile.Close()
+		log.Fatal(err)
+	}
+	if err := tmpFile.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	return tmpFile.Name(), nil
+}
+
+func Test_httpPost(t *testing.T) {
+	token := "xxx-token-yyy"
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			t.Error("Should be POST")
+		}
+		tokenPassed := r.Header.Get("Authorization")
+		tokenWanted := "token " + token
+		if tokenPassed != tokenWanted {
+			t.Error("Should pass token", tokenWanted, ", but passed: ", tokenPassed)
+		}
+	}))
+	defer srv.Close()
+
+	fileName, err := createTmp("#some title")
+	if err != nil {
+		t.Error("Should not be err", err)
+	}
+	defer os.Remove(fileName)
+
+	_, err = httpPost(srv.URL, fileName, token)
+	if err != nil {
+		t.Error("Should not be err", err)
 	}
 }

2d6ce40e92972db9ed756d25dd069f37c620d889
Chuck Grindel <chuck@chuckgrindel.com>
2022-11-07 10:04:11 -0700

    Added multiple section test

--- headerfinder_test.go
+++ headerfinder_test.go
@@ -1,7 +1,6 @@
 package ghtoc
 
 import (
-	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,17 +19,18 @@ const singleH2 = `
 </h2>
 `
 
-// func assertHeaderEqual(t *testing.T, expected, actual Header) {
-// 	if actual != expected {
-// 		t.Errorf("Unexpected header value. actual: %+#v, expected: %+#v", actual, expected)
-// 	}
-// }
+const multipleSections = `
+<h1><a id="user-content-document-title" class="anchor" aria-hidden="true" href="#document-title"><span aria-hidden="true" class="octicon octicon-link"></span></a>Document Title</h1>
+<span>Hi</span>
+<h2><a id="user-content-document-title" class="anchor" aria-hidden="true" href="#first-section"><span aria-hidden="true" class="octicon octicon-link"></span></a>First Section</h2>
+Some Text
+<h3><a id="user-content-document-title" class="anchor" aria-hidden="true" href="#first-subsection"><span aria-hidden="true" class="octicon octicon-link"></span></a>First Subsection</h3>
+<h2><a id="user-content-document-title" class="anchor" aria-hidden="true" href="#second-section"><span aria-hidden="true" class="octicon octicon-link"></span></a>Second Section</h2>
+<h4><a id="user-content-document-title" class="anchor" aria-hidden="true" href="#second-subsection"><span aria-hidden="true" class="octicon octicon-link"></span></a>Second Subsection</h4>
+`
 
 func TestFindHeaders(t *testing.T) {
 	t.Run("single H1", func(t *testing.T) {
-		// DEBUG BEGIN
-		log.Printf("*** CHUCK: ===========")
-		// DEBUG END
 		results := findHeadersInString(singleH1)
 		assert.Len(t, results, 1)
 		assert.Equal(
@@ -40,15 +40,41 @@ func TestFindHeaders(t *testing.T) {
 		)
 	})
 	t.Run("single H2", func(t *testing.T) {
-		// DEBUG BEGIN
-		log.Printf("*** CHUCK: ===========")
-		// DEBUG END
 		results := findHeadersInString(singleH2)
 		assert.Len(t, results, 1)
 		assert.Equal(
 			t,
 			Header{Depth: 1, Href: "#interesting-section", Name: "Interesting Section"},
 			results[0],
+		)
+	})
+	t.Run("multiple sections", func(t *testing.T) {
+		results := findHeadersInString(multipleSections)
+		assert.Len(t, results, 5)
+		assert.Equal(
+			t,
+			Header{Depth: 0, Href: "#document-title", Name: "Document Title"},
+			results[0],
+		)
+		assert.Equal(
+			t,
+			Header{Depth: 1, Href: "#first-section", Name: "First Section"},
+			results[1],
+		)
+		assert.Equal(
+			t,
+			Header{Depth: 2, Href: "#first-subsection", Name: "First Subsection"},
+			results[2],
+		)
+		assert.Equal(
+			t,
+			Header{Depth: 1, Href: "#second-section", Name: "Second Section"},
+			results[3],
+		)
+		assert.Equal(
+			t,
+			Header{Depth: 3, Href: "#second-subsection", Name: "Second Subsection"},
+			results[4],
 		)
 	})
 }
